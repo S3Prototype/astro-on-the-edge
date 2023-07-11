541b2014df0163c7daa428342ccf84d3e5526cd2
S3Prototype <liuqahs15@gmail.com>
2023-07-11 11:43:16 -0500

    use analytics: true

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,5 +4,7 @@ import vercel from "@astrojs/vercel/edge";
 // https://astro.build/config
 export default defineConfig({
   output: "server",
-  adapter: vercel(),
+  adapter: vercel({
+    analytics: true,
+  }),
 });
