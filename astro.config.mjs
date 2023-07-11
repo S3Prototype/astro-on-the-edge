ddb5538a4bd519286f611b85457ab4e88200f850
S3Prototype <liuqahs15@gmail.com>
2023-07-11 11:39:38 -0500

    Put a script tag

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,6 @@
 import { defineConfig } from "astro/config";
 import vercel from "@astrojs/vercel/edge";
 
-import { inject } from "@vercel/analytics";
-
-inject();
 // https://astro.build/config
 export default defineConfig({
   output: "server",
