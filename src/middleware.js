646fa14a253b987e5b8588ae30cf4adfcacfc215
S3Prototype <liuqahs15@gmail.com>
2023-12-12 11:10:07 -0600

    Added both middlewares

--- src/middleware.js
+++ src/middleware.js
@@ -1,7 +1,6 @@
 export function onRequest ({ locals, request }, next) {
   // intercept data from a request
   // optionally, modify the properties in `locals`
-  locals.test = "New title";
 
   // return a Response or the result of calling `next()`
   return next();
--- src/vercel-edge-middleware.js
+++ src/vercel-edge-middleware.js
@@ -1,7 +1,12 @@
 // Note the parameters are different from standard Astro middleware
+/**
+ *
+ * @param options.request {Request}
+ * @param options.context {import("@vercel/edge").RequestContext}
+ * @returns {object}
+ */
 export default function ({ request, context }) {
-  console.log("Hello from middleware");
-  // Return an Astro.locals object with a title property
+  // do something with request and context
   return {
     title: "Spider-man's blog",
   };
