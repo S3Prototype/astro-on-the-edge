8c9a4be28dd9e8bd341cae170bb9ca95d204cf9a
S3Prototype <liuqahs15@gmail.com>
2023-12-12 10:18:38 -0600

    Show middleware not working

--- src/vercel-edge-middleware.js
+++ src/vercel-edge-middleware.js
@@ -1,6 +1,6 @@
 // Note the parameters are different from standard Astro middleware
 export default function ({ request, context }) {
-  console.log("Test from middleware");
+  console.log("Hello from middleware");
   // Return an Astro.locals object with a title property
   return {
     title: "Spider-man's blog",
