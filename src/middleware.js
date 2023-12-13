fa6d51d354257b588ff704ed4983a36325e818d6
S3Prototype <liuqahs15@gmail.com>
2023-12-13 13:39:08 -0600

    More middleware testing

--- src/middleware.js
+++ src/middleware.js
@@ -2,7 +2,7 @@ export function onRequest ({ locals, request }, next) {
   // intercept data from a request
   // optionally, modify the properties in `locals`
 
-  locals.title = "Middleware test";
+  locals.title = "Middleware Test";
 
   // return a Response or the result of calling `next()`
   return next();
